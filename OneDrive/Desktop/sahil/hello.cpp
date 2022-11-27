e6b94856eacf7930ce71eb0464b6ebb03268f6a1
sahil <sahilvichare81@gmail.com>
2022-11-27 17:34:58 +0530

    hekkp

---
+++ OneDrive/Desktop/sahil/hello.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void bus_line(char ch)
+{
+  for (int x=80;x>0;x--)
+  cout<<ch;
+}
+
+int main(){
+
+
+bus_line('*');
+cout<<endl;
+cout<<"Bus Number: \t"<<"123"
+  <<"\nDriver: \t"<<"ABC"
+  <<"\t\tArrival Time: \t"<<"11.44"
+  <<"\tDeparture Time:"<<"12.45"
+  <<"\nFrom: \t\t"<<"xyz"
+  <<"\t\tTo: \t\t"<<"pqr"<<"\n";
+bus_line('*');
+
+
+
+}
